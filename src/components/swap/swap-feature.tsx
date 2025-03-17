b7c0befd4587393602c4ca352f180e5435a012a0
Thomas Gehrmann <s6thgehr@gmail.com>
2025-03-17 23:15:06 +0100

    Update addresses

--- src/components/swap/swap-feature.tsx
+++ src/components/swap/swap-feature.tsx
@@ -1,10 +1,14 @@
+import { Card } from "../ui/card";
 import MarketDataPage from "./market-data";
 import { SwapPanel } from "./swap-panel";
 
 export default function MatoFeature() {
   return (
     <div className="flex flex-col-reverse lg:flex-row gap-8 justify-center py-24">
-      <MarketDataPage />
+      {/* <MarketDataPage /> */}
+      <Card className="w-full lg:w-1/2 sm:min-h-96 text-center pt-16 text-2xl font-bold">
+        Price Chart coming soon
+      </Card>
       <SwapPanel />
     </div>
   );
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,13 +5,13 @@ export const VOLUME_PRECISION = 1_000_000_000;
 
 // Mint address
 export const USDC_MINT = new PublicKey(
-  "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"
+  "GbkB1LkDEHi2B9eQ6zmjGfrRs1KTpD7ZqzLs27Lm7et8"
 );
 
 // Program accounts
 export const EXITS_ADDRESS = new PublicKey(
-  "D467xRNpNHvxbG7nRApDSshnvqVDhL4YjBYqz9TsoKF9"
+  "ASgjENhoPdnC5U5cf5oNrzP6HsBXz2wyNpjW2HSe43Uh"
 );
 export const PRICES_ADDRESS = new PublicKey(
-  "Dpe9rm2NFSTowGbvrwXccbW7FtGfrQCdu6ogugNW6akK"
+  "F5UWZGomBEEGFfs44TTyNR2Xi2WLrue8TPoRzMvmWZup"
 );
