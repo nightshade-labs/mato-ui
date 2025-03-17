649335de5250c759142cef3fcfb0eee53d1454c7
Thomas Gehrmann <s6thgehr@gmail.com>
2025-03-18 00:10:40 +0100

    Adjust explorer link to sonic

--- src/components/cluster/cluster-data-access.tsx
+++ src/components/cluster/cluster-data-access.tsx
@@ -103,7 +103,7 @@ export function ClusterProvider({ children }: { children: ReactNode }) {
     },
     setCluster: (cluster: Cluster) => setCluster(cluster),
     getExplorerUrl: (path: string) =>
-      `https://explorer.solana.com/${path}${getClusterUrlParam(cluster)}`,
+      `https://explorer.sonic.game/${path}?cluster=testnet.v1`,
   };
   return <Context.Provider value={value}>{children}</Context.Provider>;
 }
